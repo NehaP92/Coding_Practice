78b5c7665f2f193172e12cd8c8b9d7a21334e5f8
Neha <neha.educational@gmail.com>
2021-08-08 23:42:29 -0500

    ggplot - R

---
+++ R/ggplot.R
@@ -0,0 +1,87 @@
+library(readr)
+library(ggplot2)
+tips <- read_csv("tips.csv")
+
+ggplot(tips)
+
+ggplot(tips, aes(x=total_bill, y=tip))
+
+ggplot(tips,aes(x=total_bill, y=tip)) + geom_point()
+
+ggplot(tips, aes(x=total_bill, y=tip, color=time)) + geom_point()
+
+ggplot(tips, aes(x=total_bill, y=tip, color=time, shape=smoker)) + geom_point()
+
+ggplot(tips, aes(x=total_bill, y=tip, color=time, shape=smoker,)) + 
+  geom_point() + facet_wrap(~day)
+
+
+ggplot(tips, aes(x=day)) + geom_bar()
+
+ggplot(tips, aes(x=day, fill=smoker)) + geom_bar()
+
+ggplot(tips, aes(x=day, fill = smoker)) + geom_bar(position = "dodge")
+
+ggplot(tips, aes(x=day, fill = smoker)) + geom_bar(position = "fill")
+
+ggplot(tips, aes(x=day, fill = smoker)) + geom_bar(position = "dodge") +
+  facet_wrap(~time)
+
+
+
+
+
+ggplot(tips, aes(x=day, fill = smoker)) + geom_bar(position = "dodge") +
+  facet_wrap(~time) +
+  labs(title = "Number of Patrons by Day and Meal", 
+       subtitle = "Position by Smoker",
+       x="Day", y="Count")
+
+
+
+
+
+
+tips %>%
+  select(smoker,tip,day,time) %>%
+  mutate(gbp_tip = tip*0.82) %>%
+  filter(gbp_tip>5) %>%
+  ggplot(aes(x=day,fill=smoker)) + geom_bar(position="dodge") +
+  facet_wrap(~time) +
+  labs(title="Number of Patrons by Day and Meal",
+       subtitle = "Position by Smoker",
+       x="Day",y="Count")
+
+
+
+
+
+
+tips %>%
+  select(smoker, tip, day, time) %>%
+  mutate(gbp_tip = tip * .82) %>%
+  filter(gbp_tip >= 5) %>%
+  ggplot(aes(x = day, fill= smoker)) +
+  geom_bar(position = "dodge") + 
+  facet_wrap(~time) +
+  labs(title = "Number of Patrons By Day and Meal",
+       subtitle = "Position by Smoker",
+       x =  "Day", y = "Count")
+
+
+
+
+library(dplyr)
+
+
+
+
+tips %>%
+  select(smoker,tip,day,time) %>%
+  mutate(gbp_tip = tip*0.82) %>%
+  filter(gbp_tip>5) %>%
+  ggplot(aes(x=day,fill=smoker)) + geom_bar(position="dodge") +
+  facet_wrap(~time) +
+  labs(title="Number of Patrons by Day and Meal",
+       subtitle = "Position by Smoker",
+       x="Day",y="Count")
